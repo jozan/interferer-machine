54a3ba10a910964df767ec33c0e2272e6fa16902
Johan Ruokangas <johan.ruokangas@fraktio.fi>
2022-12-04 16:42:29 +0200

    feat: periodically send data to server

--- ee.mock.ts
+++ ee.mock.ts
@@ -11,44 +11,58 @@ const WEBSOCKET_URL =
     return value
   }) || 'ws://127.0.0.1:8080'
 
-console.log('Empty Epsilon mock server started!')
+function connectWebSocket() {
+  console.log('[EE MOCK] connecting to websocket server...')
+  const ws = new WebSocket(WEBSOCKET_URL)
 
-// connect to the websocket server
-// try to reconnect if the connection is lost
-const ws = new WebSocket(WEBSOCKET_URL)
+  ws.addEventListener('message', (e) => {
+    const message = JSON.parse(e.data)
+    console.log('[EE MOCK]: Received message:', message)
+  })
 
-ws.addEventListener('message', (e) => {
-  const message = JSON.parse(e.data)
-  console.log('[EE MOCK]: Received message:', message)
-})
+  ws.addEventListener('open', (e) => {
+    console.log(`[EE MOCK]: Connected to the websocket server: ${ws.url}`)
+    sendData()
+  })
 
-ws.addEventListener('open', (e) => {
-  console.log(`[EE MOCK]: Connected to the websocket server: ${ws.url}`)
-})
+  ws.addEventListener('error', (e) => {
+    console.log('[EE MOCK]: Error:', e)
+  })
 
-ws.addEventListener('error', (e) => {
-  console.log('[EE MOCK]: Error:', e)
-})
+  ws.addEventListener('close', (e) => {
+    console.log('[EE MOCK]: Disconnected from the websocket server')
+    setTimeout(() => {
+      console.log('[EE MOCK]: Reconnecting to websocket server...')
+      connectWebSocket()
+    }, 1000)
+  })
 
-ws.addEventListener('close', (e) => {
-  console.log('[EE MOCK]: Disconnected from the websocket server')
-})
+  function sendData() {
+    ws.send(JSON.stringify(data))
+    setTimeout(sendData, 1000)
+  }
+}
+
+connectWebSocket()
 
 // open up the server so mock process is long lived
+console.log('[EE MOCK]: Empty Epsilon mock server started!')
 Bun.serve({
   port: PORT,
   fetch(req) {
-    return new Response('Empty Epsilon mock server is running')
+    return new Response('[EE MOCK]: Empty Epsilon mock server is running')
   },
   error(error: Error) {
-    return new Response('Uh oh!!\n' + error.toString(), { status: 500 })
+    return new Response('[EE MOCK]: Uh oh!!\n' + error.toString(), {
+      status: 500
+    })
   },
   websocket: {
     close(ws) {
-      console.log('Server: Client disconnected')
+      console.log('[EE MOCK]: Client disconnected')
     },
     open(ws) {
-      console.log('Server: Client connected')
+      console.log('[EE MOCK]: Client connected')
     },
     message(ws, message) {
       console.log(message)
